0227a6651fac92c8be901c536ab9b24673615759
maDU59_ <matthias.ducourant@gmail.com>
2026-02-21 23:14:32 +0100

    Added a queue system for skin generation

--- scripts/backend.py
+++ scripts/backend.py
@@ -3,11 +3,15 @@ from utils.skin_utils import get_output_local, get_skin_local
 from dotenv import load_dotenv
 from datetime import timedelta
 import skin_gen
+import queue
 import os
 import uuid
+import threading
 
 load_dotenv()
 
+skin_queue = queue.Queue()
+results = {}
 app = Flask(__name__)
 app.secret_key = os.getenv("SESSION_KEY")
 
@@ -33,11 +37,25 @@ def search(username = "a"):
 
 @app.route('/generate', methods=['POST'])
 def generate():
+    uuid = get_uuid()
+    if uuid in results:
+        print(uuid, " made a request while already in queue")
+        return jsonify({
+            "status": "already_queued",
+            "result": uuid
+        }), 202
+
     prompt = request.json.get('prompt')
-    
-    skin_gen.generate_skin(prompt, get_uuid())
-    
-    return jsonify({"success": True, "img_url": get_output_local(get_uuid())})
+
+    skin_queue.put((prompt, uuid))
+
+    results[uuid] = {"status": "queued",
+        "result": "None"}
+
+    return jsonify({
+        "status": "queued",
+        "result": uuid
+    }), 202
 
 @app.route('/get-skin', methods=['POST'])
 def get_skin_serv():
@@ -47,10 +65,67 @@ def get_skin_serv():
         "skin": skin[1]
     })
 
+@app.route('/check-queue')
+def is_in_queuue():
+    uuid = get_uuid()
+    return jsonify({
+        "status": uuid in results,
+        "uuid": uuid
+    })
+
+@app.route('/result/<ticket_id>')
+def get_result(ticket_id):
+    data = results.get(ticket_id)
+    
+    if not data:
+        return jsonify({
+            "status": "error", 
+            "result": "Ticket expired or not found"
+            }), 404
+
+    if data["status"] == "completed":
+        final_data = results.pop(ticket_id) 
+        return jsonify({
+            "status": "completed", 
+            "result": f"{final_data['result']}"
+        })
+    elif data["status"] == "failed":
+        return jsonify({
+            "status": "error", 
+            "result": "Error while generating skin"
+            }), 404
+    
+    return jsonify(data), 202
+
 def get_uuid():
     if "uuid" not in session:
         session["uuid"] = str(uuid.uuid4())
     return session["uuid"]
+
+def worker():
+    """
+    Generate skins
+    """
+    while True:
+        # This blocks until an item is available
+        prompt, uuid = skin_queue.get()
+        
+        results[uuid]["status"] = "processing"
+        
+        try:
+            print(f"Processing: {uuid}")
+            skin_gen.generate_skin(prompt, uuid)
+            
+            results[uuid]["result"] = get_output_local(uuid)
+            results[uuid]["status"] = "completed"
+            print(f"Finished processing: {uuid}")
+        except Exception as e:
+            results[uuid]["status"] = "failed"
+        
+        skin_queue.task_done()
+
+worker_thread = threading.Thread(target=worker, daemon=True)
+worker_thread.start()
 
 if __name__ == "__main__":
     #host 0.0.0.0 to access it on other devices on the same network, not suited for production
